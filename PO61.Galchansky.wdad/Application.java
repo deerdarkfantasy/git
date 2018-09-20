830a0229109cf61efda55af08fe04ccdd2f25027
deepdarkfantasy <life.knife.00@gmail.com>
2018-09-20 10:36:05 +0400

    Added entry point

--- PO61.Galchansky.wdad/Application.java
+++ PO61.Galchansky.wdad/Application.java
@@ -2,6 +2,6 @@ package PO61.Galchansky.wdad;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("I’m Galchansky alexander, and I’m not a monkey");
+        System.out.println("I’m Galchansky Alexander, and I’m not a monkey");
     }
 }
